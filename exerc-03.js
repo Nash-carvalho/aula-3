93113affeb9f7533e6baf17cdd742a7982d4a895
Nash-carvalho <feprofeta3@gmail.com>
2025-07-01 14:25:43 -0300

    Update exerc-03.js

--- exerc-03.js
+++ exerc-03.js
@@ -1,10 +1,10 @@
 
 let valor = 7
-valor += 3
+valor += 3               (SOMAR 3 de uma vez)
 console.log(valor)
 
-valor++ 
+valor++                 (SOMAR mais 1 )
 console.log(valor)
 
-valor -=2 
+valor -=2             (SUBITRAIR 2 de uma vez )
 console.log(valor)
